62548985071aaf24403c024ebaa5c1b9ec06c13e
Luka <ggflyte@mail.ru>
2024-01-28 13:04:17 +0300

    login

---
+++ src/components/LoginUser.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="d-flex justify-center">
+        <v-card width="600px" class="mt-12 pa-10">
+            <v-card-title>
+                Войдите в аккаунт
+            </v-card-title>
+
+            <v-text-field
+                label="Введите логин"
+                v-model="login"
+                outlined
+            ></v-text-field>
+
+            <v-text-field
+                label="Введите пароль"
+                v-model="password"
+                outlined
+            ></v-text-field>
+
+            <v-btn @click="authenticate">
+                Войти
+            </v-btn>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "LoginUser",
+    data:()=>{
+        return{
+            login: "login",
+            password: "password",
+
+        }
+
+    },
+    methods:{
+        authenticate(){
+            console.log(this.login, this.password)
+
+        }
+
+    }
+
+}
+
+
+
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <LoginUser/>
+</template>
+
+<script>
+import LoginUser from '@/components/LoginUser.vue'
+export default{
+  components:{
+    LoginUser,
+
+  }
+
+}
+
+</script>
